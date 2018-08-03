806dd37c6e4962b2b3e3aa6e94a027d9f168de4e
johtela <tommi.johtela@dnainternet.net>
2018-08-03 15:54:06 +0300

    Added template for creating literate .NET Standard library.

---
+++ Templates/content/Class1.cs
@@ -0,0 +1,26 @@
+﻿/*
+# Introduction
+
+This is a simple example on how to create code with [literate comments]
+(https://johtela.github.io/LiterateCS/Introduction.html#documentation-in-comments).
+You can interleave documentation with the code and `literatecs` will compile a
+HTML page from it.
+*/
+namespace LiterateLib
+{
+	/*
+	Documentation can be added to any place in the code. Use multi-line comments
+	delimited by `/*` and <code>&ast;/</code>. All the formatting options of markdown
+	are available.
+	*/
+    public class Class1
+    {
+		/*
+		You can also create macros by embedding code into regions. You can refer
+		to the code snippets in the [markdown files](index.html).
+		*/
+		#region Add function
+		public int Add (int x, int y) => x + y;
+		#endregion
+	}
+}
