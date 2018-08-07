b38cc229ca5ae988d64b527ab194ed303300d3cc
johtela <tommi.johtela@dnainternet.net>
2018-08-07 12:32:48 +0300

    Sidebar now closes when a link in it is clicked.

--- docs/sidebar/sidebar.js
+++ docs/sidebar/sidebar.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     var sidebarMainMenu = $('#header-menu ul:first');
     var staticContent = $('#static-content');
     staticContent.find('h1').each(function () {
-        sidebarMainMenu.append('<li id="' + $(this).attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
+        sidebarMainMenu.append('<li id="' + $(this).attr('id') + '-menu"><a class="page-link" href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
         title = sidebarMainMenu.find('#' + $(this).attr('id'));
     });
     for (var i = 2; i <= 3; i++) {
-        AddHeaderLevel(i);
+        addHeaderLevel(i);
     }
 
-    function AddHeaderLevel (level)
+    function addHeaderLevel (level)
     {
         staticContent.find('h' + level).each(function () {
             prevTitle = sidebarMainMenu.find('#' + $(this)
@@ -18,32 +18,49 @@ $(document).ready(function () {
                 .attr('id').replace('.', '\\.') + '-menu');
             prevTitle.not(":has(ul)").append('<ul class="sub-menu"></ul>');
             prevTitle.find('.sub-menu').append('<li id="' + $(this)
-                .attr('id') + '-menu"><a href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
+                .attr('id') + '-menu"><a class="page-link" href="#' + $(this).attr('id') + '">' + $(this).html() + '</li>');
         });
     }
 
     // Sidebar toggling
+    var sidebarVisible = "col-sm-4 col-xs-6 sidebar-popup";
+    var sidebarHidden = "hidden-sm hidden-xs";
+    var closeArrow = "fa-angle-double-left";
+    var openArrow = "fa-angle-double-right";
+
+    function isSidebarOpen() {
+        return $("#sidebar").hasClass("col-sm-4");
+    }
+
+    function closeSidebar() {
+        $("#sidebar").removeClass(sidebarVisible)
+            .addClass(sidebarHidden);
+        $("#sidebar-toggle-icon").removeClass(closeArrow)
+            .addClass(openArrow);
+    }
+
+    function openSidebar() {
+        $("#sidebar").removeClass(sidebarHidden)
+            .addClass(sidebarVisible);
+        $("#sidebar-toggle-icon").removeClass(openArrow)
+            .addClass(closeArrow);
+    }
+
     $("#sidebar-toggle").click(function(e) {
         e.preventDefault();
-        if ($("#sidebar").is(":visible") && !$("#sidebar").hasClass("col-sm-4"))
+        if ($("#sidebar").is(":visible") && !isSidebarOpen ())
             return;
-        var sidebarVisible = "col-sm-4 col-xs-6 sidebar-popup";
-        var sidebarHidden = "hidden-sm hidden-xs";
-        var closeArrow = "fa-angle-double-left";
-        var openArrow = "fa-angle-double-right";
-
-		if ($("#sidebar").hasClass("col-sm-4")) {
-            $("#sidebar").removeClass (sidebarVisible)
-                .addClass(sidebarHidden);
-            $("#sidebar-toggle-icon").removeClass(closeArrow)
-                .addClass(openArrow);
+        if (isSidebarOpen()) {
+            closeSidebar();
 		}
-		else {
-            $("#sidebar").removeClass (sidebarHidden)
-                .addClass(sidebarVisible);
-            $("#sidebar-toggle-icon").removeClass(openArrow)
-                .addClass(closeArrow);
+        else {
+            openSidebar();
             window.scrollTo(0, 0);
 		}
+    });
+
+    $(".page-link").click(function (e) {
+        if (isSidebarOpen ())
+            closeSidebar();
     });
 });
